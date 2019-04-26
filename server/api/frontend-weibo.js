3f8bf91af2760771ed18ed711362d529da09f8a9
林岑影 <lincenying@qq.com>
2019-04-26 22:58:23 +0800

    up

--- server/api/frontend-weibo.js
+++ server/api/frontend-weibo.js
@@ -199,7 +199,7 @@ exports.detail = async (req, res) => {
 }
 
 exports.checkUpdate = (req, res) => {
-    const jsonTxt = fs.readFileSync('./service/config/app.json', 'utf-8')
+    const jsonTxt = fs.readFileSync('./server/config/app.json', 'utf-8')
     const json = JSON.parse(jsonTxt)
     res.json({ code: 200, data: json })
 }
