660ec2e282aad512a9185b7fbb02a7d3b45ca23d
林岑影 <lincenying@qq.com>
2019-08-09 09:39:51 +0800

    up

--- server/routes/is-user.js
+++ server/routes/is-user.js
@@ -12,6 +12,7 @@ module.exports = async (req, res, next) => {
             res.cookie('user', '', { maxAge: 0 })
             res.cookie('userid', '', { maxAge: 0 })
             res.cookie('username', '', { maxAge: 0 })
+            res.cookie('useremail', '', { maxAge: 0 })
             return res.json({
                 code: -400,
                 message: '登录验证失败',
