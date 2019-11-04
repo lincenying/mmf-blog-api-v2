3fcf5156d29d8b84d4dd263fafd2beb3e39b4e70
林岑影 <lincenying@qq.com>
2019-11-04 15:13:48 +0800

    no message

--- app.js
+++ app.js
@@ -56,15 +56,15 @@ app.get('*', (req, res) => {
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
-})
+// app.use(function(req, res, next) {
+//     const err = new Error('Not Found')
+//     err.status = 404
+//     next(err)
+// })
 
-app.use(function(err, req, res) {
-    res.status(err.status || 500)
-    res.send(err.message)
-})
+// app.use(function(err, req, res) {
+//     res.status(err.status || 500)
+//     res.send(err.message)
+// })
 
 module.exports = app
