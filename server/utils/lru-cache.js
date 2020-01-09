2eb1c09c6c290ece209cb6678ba0dcbd1632433f
林岑影 <lincenying@qq.com>
2020-01-09 21:52:46 +0800

    up

--- server/utils/lru-cache.js
+++ server/utils/lru-cache.js
@@ -7,9 +7,9 @@ const meizituCache = new lruCache({
 
 exports.meizituCache = meizituCache
 
-const xiguaCache = new lruCache({
+const douyinCache = new lruCache({
     max: 1000,
-    maxAge: 1000 * 60 * 60 * 24 * 1
+    maxAge: 1000 * 60 * 60 * 1
 })
 
-exports.xiguaCache = xiguaCache
+exports.douyinCache = douyinCache
