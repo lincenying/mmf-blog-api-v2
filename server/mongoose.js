4cc06170856ccd7716a15633cb3a2b90ee65544f
林岑影 <lincenying@qq.com>
2023-02-12 17:25:54 +0800

    commit on 2023-02-12 17:25:54

--- server/mongoose.js
+++ server/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const mongoUrl = 'localhost'
+mongoose.set('strictQuery', false)
 mongoose.connect(`mongodb://${mongoUrl}/mmfblog_v2`, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise
 module.exports = mongoose
